6fd55583849b64415d9123463ac304765997e80a
Roma K <romankuchukov@gmail.com>
2020-11-04 18:26:12 -0500

    cleaned up data

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
+import topology from './data';
+
 const width = 960;
 const height = 500;
 const radius = 20;
 
-const topology = hexTopology(radius, width, height);
 const projection = hexProjection(radius);
 
 const path = d3.geo.path().projection(projection);
 
-const svg = d3.select('body').append('svg')
-//.attr('width', width).attr('height', height);
-.attr('preserveAspectRatio', 'xMinYMin meet').attr('viewBox', `0 0 ${width} ${height}`)
-//preserveAspectRatio="xMinYMin meet" viewBox="0 0 960 500"
+const svg = d3.select('body')
+  .append('svg')
+  .attr('preserveAspectRatio', 'xMinYMin meet')
+  .attr('viewBox', `0 0 ${width} ${height}`)
+
+
 svg.append('g')
   .attr('class', 'hexagon')
   .selectAll('path')
@@ -51,38 +54,6 @@ function mouseup() {
 
 function redraw(border) {
   border.attr('d', path(topojson.mesh(topology, topology.objects.hexagons, function(a, b) { return a.fill ^ b.fill; })));
-}
-
-function hexTopology(radius, width, height) {
-  const dx = radius * 2 * Math.sin(Math.PI / 3);
-  const dy = radius * 1.5;
-  const m = Math.ceil((height + radius) / dy) + 1;
-  const n = Math.ceil(width / dx) + 1;
-  const geometries = [];
-  const arcs = [];
-
-  for (let j = -1; j <= m; ++j) {
-    for (let i = -1; i <= n; ++i) {
-      const y = j * 2, x = (i + (j & 1) / 2) * 2;
-      arcs.push([[x, y - 1], [1, 1]], [[x + 1, y], [0, 1]], [[x + 1, y + 1], [-1, 1]]);
-    }
-  }
-
-  for (let j = 0, q = 3; j < m; ++j, q += 6) {
-    for (let i = 0; i < n; ++i, q += 3) {
-      geometries.push({
-        type: 'Polygon',
-        arcs: [[q, q + 1, q + 2, ~(q + (n + 2 - (j & 1)) * 3), ~(q - 2), ~(q - (n + 2 + (j & 1)) * 3 + 2)]],
-        fill: Math.random() > i / n * 2
-      });
-    }
-  }
-
-  return {
-    transform: {translate: [0, 0], scale: [1, 1]},
-    objects: {hexagons: {type: 'GeometryCollection', geometries: geometries}},
-    arcs: arcs
-  };
 }
 
 function hexProjection(radius) {
