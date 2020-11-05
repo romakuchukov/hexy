22e9dc8abfae72fe291f00e0598757f2ba70b57c
Roma K <romankuchukov@gmail.com>
2020-11-05 12:40:49 -0500

    converting to arrow functions

--- src/main.js
+++ src/main.js
@@ -49,24 +49,22 @@ svg.append('path')
   .attr('class', 'mesh')
   .attr('d', path);
 
-const border = svg.append('path').attr('class', 'border').call(redraw);
-
-function redraw(border) {
+const redraw = (border) => {
   border.attr('d', path(topojson.mesh(topology, topology.objects.hexagons, (a, b) => a.fill ^ b.fill)));
-}
+};
+
+const border = svg.append('path').attr('class', 'border').call(redraw);
 
 function hexProjection(radius) {
   const dx = radius * 2 * Math.sin(Math.PI / 3);
   const dy = radius * 1.5;
   return {
-    stream: function(stream) {
-      return {
-        point: function(x, y) { stream.point(x * dx / 2, (y - (2 - (y & 1)) / 3) * dy / 2); },
-        lineStart: function() { stream.lineStart(); },
-        lineEnd: function() { stream.lineEnd(); },
-        polygonStart: function() { stream.polygonStart(); },
-        polygonEnd: function() { stream.polygonEnd(); }
-      };
-    }
+    stream: (stream) => ({
+        point: (x, y) => { stream.point(x * dx / 2, (y - (2 - (y & 1)) / 3) * dy / 2); },
+        lineStart: () => { stream.lineStart(); },
+        lineEnd: () => { stream.lineEnd(); },
+        polygonStart: () => { stream.polygonStart(); },
+        polygonEnd: () => { stream.polygonEnd(); }
+    })
   };
 }
