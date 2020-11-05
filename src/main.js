c75998d7c8b7645158e1528fe2756b9e7b18ad4a
Roma K <romankuchukov@gmail.com>
2020-11-05 09:46:25 -0500

    converted css to scss

--- src/main.js
+++ src/main.js
@@ -26,21 +26,6 @@ function mouseup(e) {
   mousing = 0;
 }
 
-function hexProjection(radius) {
-  const dx = radius * 2 * Math.sin(Math.PI / 3);
-  const dy = radius * 1.5;
-  return {
-    stream: function(stream) {
-      return {
-        point: function(x, y) { stream.point(x * dx / 2, (y - (2 - (y & 1)) / 3) * dy / 2); },
-        lineStart: function() { stream.lineStart(); },
-        lineEnd: function() { stream.lineEnd(); },
-        polygonStart: function() { stream.polygonStart(); },
-        polygonEnd: function() { stream.polygonEnd(); }
-      };
-    }
-  };
-}
 
 const svg = d3.select('body')
   .append('svg')
@@ -68,4 +53,20 @@ const border = svg.append('path').attr('class', 'border').call(redraw);
 
 function redraw(border) {
   border.attr('d', path(topojson.mesh(topology, topology.objects.hexagons, (a, b) => a.fill ^ b.fill)));
+}
+
+function hexProjection(radius) {
+  const dx = radius * 2 * Math.sin(Math.PI / 3);
+  const dy = radius * 1.5;
+  return {
+    stream: function(stream) {
+      return {
+        point: function(x, y) { stream.point(x * dx / 2, (y - (2 - (y & 1)) / 3) * dy / 2); },
+        lineStart: function() { stream.lineStart(); },
+        lineEnd: function() { stream.lineEnd(); },
+        polygonStart: function() { stream.polygonStart(); },
+        polygonEnd: function() { stream.polygonEnd(); }
+      };
+    }
+  };
 }
