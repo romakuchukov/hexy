f81e2b8efd414ab7f10e465184fa1b7a6a9cf323
Roma K <romankuchukov@gmail.com>
2021-09-14 15:34:25 -0400

    creted utils and moved funtions there

--- src/main.js
+++ src/main.js
@@ -1,81 +1,12 @@
 //https://bl.ocks.org/mbostock/5249328
-import { primer } from './data';
 import { mesh } from 'topojson';
+import { getArcs, hexProjection, hexTopology } from './util';
 
-const cloneArray = (array) => {
-  const clone = [];
+const svg = d3.select('body').append('svg');
+const border = svg.append('path').attr('class', 'border');
 
-  for(const item of array) {
-    clone.push((typeof(item)==='object') ? cloneArray(item) : item);
-  }
-
-  return clone;
-};
-
-const getArcs = ({instances, rows}) => {
-
-  const clonedPrimer = cloneArray(primer.arcs.slice(1));
-  const arcs = [];
-
-  arcs.push(cloneArray(clonedPrimer));
-
-
-  while (instances--) {
-    clonedPrimer.forEach(item => {
-      item[0][0] = item[0][0];
-    });
-    arcs.push(cloneArray(clonedPrimer));
-  }
-
-  let clonedArcs = cloneArray(arcs);
-
-  while (rows--) {
-
-    clonedArcs.forEach(item => {
-        item.forEach(val => {
-          //console.log(val)
-          val[0][0] = val[0][0];
-          val[0][1] = val[0][1];
-        });
-        arcs.push(cloneArray(item));
-    });
-  }
-
-  return arcs.flat();
-};
-
-const hexTopology = ({ instances, rows }) => {
-  const sides = 6;
-  const polygons = sides*(instances)*(rows);
-  const hexArcs = [];
-  let positions = [];
-
-  for (let i = 1; i <= polygons; i++) {
-    positions.push(i);
-
-    if((i % sides) === 0 && i > 0) {
-      hexArcs.push({ type: 'Polygon', arcs: [positions], fill: false });
-      positions = [];
-      positions.push(i-1);
-    }
-  }
-
-  return hexArcs;
-};
-
-const hexProjection = radius => {
-  const dx = radius * 2 * Math.sin(Math.PI / 3);
-  const dy = radius * 1.5;
-  return {
-    stream: stream => ({
-      point: (x, y) => { stream.point(x * dx / 2, (y - (2 - (y & 1)) / 3) * dy / 2); },
-      lineStart: () => { stream.lineStart(); },
-      lineEnd: () => { stream.lineEnd(); },
-      polygonStart: () => { stream.polygonStart(); },
-      polygonEnd: () => { stream.polygonEnd(); }
-    })
-  };
-};
+const projection = hexProjection(20);
+const path = d3.geoPath(projection);
 
 const width = 960;
 const height = 500;
@@ -98,16 +29,15 @@ const topology = {
   ]
 };
 
-const projection = hexProjection(20);
-const path = d3.geoPath(projection);
-
 const onClick = (e, data) => {
   data.fill = !data.fill;
   d3.select(e.target).classed('fill', data.fill);
   border.call(redraw);
 }
 
-const svg = d3.select('body').append('svg');
+const redraw = border => {
+  border.attr('d', path(mesh(topology, topology.objects.hexagons, (a, b) => a.fill ^ b.fill )));
+};
 
 svg.attr('viewBox', `0 0 ${width} ${height}`).attr('preserveAspectRatio', 'xMinYMin meet');
 
@@ -127,9 +57,3 @@ svg.append('path')
   //.enter()
   .attr('d', d => `${path(mesh(topology, d))}Z`)
   .attr('class', 'mesh')
-
-const border = svg.append('path').attr('class', 'border');
-
-const redraw = border => {
-  border.attr('d', path(mesh(topology, topology.objects.hexagons, (a, b) => a.fill ^ b.fill )));
-};
---
+++ src/util.js
@@ -0,0 +1,76 @@
+import { primer } from './data';
+
+const cloneArray = (array) => {
+  const clone = [];
+
+  for(const item of array) {
+    clone.push((typeof(item)==='object') ? cloneArray(item) : item);
+  }
+
+  return clone;
+};
+
+export const getArcs = ({instances, rows}) => {
+
+  const clonedPrimer = cloneArray(primer.arcs.slice(1));
+  const arcs = [];
+
+  arcs.push(cloneArray(clonedPrimer));
+
+
+  while (instances--) {
+    clonedPrimer.forEach(item => {
+      item[0][0] = item[0][0];
+    });
+    arcs.push(cloneArray(clonedPrimer));
+  }
+
+  let clonedArcs = cloneArray(arcs);
+
+  while (rows--) {
+
+    clonedArcs.forEach(item => {
+        item.forEach(val => {
+          //console.log(val)
+          val[0][0] = val[0][0];
+          val[0][1] = val[0][1];
+        });
+        arcs.push(cloneArray(item));
+    });
+  }
+
+  return arcs.flat();
+};
+
+export const hexProjection = (radius) => {
+  const dx = radius * 2 * Math.sin(Math.PI / 3);
+  const dy = radius * 1.5;
+  return {
+    stream: stream => ({
+      point: (x, y) => { stream.point(x * dx / 2, (y - (2 - (y & 1)) / 3) * dy / 2); },
+      lineStart: () => { stream.lineStart(); },
+      lineEnd: () => { stream.lineEnd(); },
+      polygonStart: () => { stream.polygonStart(); },
+      polygonEnd: () => { stream.polygonEnd(); }
+    })
+  };
+};
+
+export const hexTopology = ({ instances, rows }) => {
+  const sides = 6;
+  const polygons = sides*(instances)*(rows);
+  const hexArcs = [];
+  let positions = [];
+
+  for (let i = 1; i <= polygons; i++) {
+    positions.push(i);
+
+    if((i % sides) === 0 && i > 0) {
+      hexArcs.push({ type: 'Polygon', arcs: [positions], fill: false });
+      positions = [];
+      positions.push(i-1);
+    }
+  }
+
+  return hexArcs;
+};
