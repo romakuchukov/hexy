388e3187d5ca19e8c93026fc45428ac7afb4f7fe
Roma K <romankuchukov@gmail.com>
2021-09-15 23:45:30 -0400

    removed remnants of rollup

--- prod.rollup.config.js
+++
@@ -1,8 +0,0 @@
-import configs from './rollup.config';
-import { terser } from 'rollup-plugin-terser';
-
-configs.forEach(config => {
-  Object.assign(config, { plugins: [...config.plugins, terser()] });
-});
-
-export default configs;
